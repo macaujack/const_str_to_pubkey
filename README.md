ead85c31094d85ffb7dcd0774b2de959bac1b5d6
Yan <yanxfu@gmail.com>
2024-03-06 18:21:12 +0800

    fix: public string can't be empty
    * doc: fix some mistakes in docs

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # const_str_to_pubkey
 
-A Rust library that helps developers derive a **constant** `Pubkey` with a **constant** string. (Base58 decode algorithm is the same with [bs58-rs](https://github.com/Nullus157/bs58-rs))
+A Rust library that helps developers derive a **constant** Solana `Pubkey` with a **constant** string. (Base58 decode algorithm is the same with [bs58-rs](https://github.com/Nullus157/bs58-rs))
 
 We sometimes want to declare a constant `Pubkey` (e.g., to store the admin's public key). We can use [`pubkey!`](https://docs.rs/solana-program/latest/solana_program/macro.pubkey.html) macro provided by Solana official library. However, this macro only works with string literals. If we have a constant `&'static str` (e.g., from the `env!` macro), we can't derive the constant `Pubkey` with `pubkey!` macro.
 
@@ -23,6 +23,8 @@ ADMIN_PUBKEY=AdminPubkey11111111111111111111111111111111 anchor build
 # or exports the environment variable
 ```
 
+Or create a cargo configuration file and store the admin's public key there (see the following section).
+
 ## Cargo check error
 
 When running `cargo check` (sometimes rust-analyzer runs for us), we may get an error, saying the environment variable is not set. This is because cargo doesn't recognize the environment variables we've set.
@@ -33,5 +35,3 @@ To solve this issue, we can simply add a cargo configuration file `.cargo/config
 [env]
 ADMIN_PUBKEY = "AdminPubkey11111111111111111111111111111111"
 ```
-
-Note we can use an arbitrary valid public key string here. It doesn't need to be the real admin public key, because this is only to satisfy cargo. When compiling, we still need to specify the environment variable. The compiler won't read environment variables from cargo configuration file. Therefore, it's safe and nice to check in this file with version control system like git.
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 //! # const_str_to_pubkey
 //!
-//! A Rust library that helps developers derive a **constant** `Pubkey` with a **constant** string. (Base58 decode algorithm is the same with [`bs58` crate](https://docs.rs/bs58/latest/bs58/))
+//! A Rust library that helps developers derive a **constant** Solana `Pubkey` with a **constant** string. (Base58 decode algorithm is the same with [`bs58` crate](https://docs.rs/bs58/latest/bs58/))
 //!
 //! We sometimes want to declare a constant `Pubkey` (e.g., to store the admin's public key). We can use [`pubkey!`](https://docs.rs/solana-program/latest/solana_program/macro.pubkey.html) macro provided by Solana official library. However, this macro only works with string literals. If we have a constant `&'static str` (e.g., from the `env!` macro), we can't derive the constant `Pubkey` with `pubkey!` macro.
 //!
@@ -8,7 +8,7 @@
 //!
 //! ## Usage
 //!
-//! ```rust
+//! ```ignore
 //! use const_str_to_pubkey::str_to_pubkey;
 //!
 //! const ADMIN_PUBKEY: Pubkey = str_to_pubkey(env!("ADMIN_PUBKEY"));
@@ -23,18 +23,18 @@
 //! # or exports the environment variable
 //! ```
 //!
+//! Or create a cargo configuration file and store the admin's public key there (see the following section).
+//!
 //! ## Cargo check error
 //!
 //! When running `cargo check` (sometimes rust-analyzer runs for us), we may get an error, saying the environment variable is not set. This is because cargo doesn't recognize the environment variables we've set.
 //!
 //! To solve this issue, we can simply add a cargo configuration file `.cargo/config.toml` at project/workspace root (for details, refer to [The Cargo Book](https://doc.rust-lang.org/cargo/reference/config.html)), with content:
 //!
-//! ```
+//! ```ignore
 //! [env]
 //! ADMIN_PUBKEY = "AdminPubkey11111111111111111111111111111111"
 //! ```
-//!
-//! Note we can use an arbitrary valid public key string here. It doesn't need to be the real admin public key, because this is only to satisfy cargo. When compiling, we still need to specify the environment variable. The compiler won't read environment variables from cargo configuration file. Therefore, it's safe and nice to check in this file with version control system like git.
 
 use solana_program::pubkey::Pubkey;
 
@@ -102,6 +102,7 @@ pub const fn str_to_pubkey(s: &'static str) -> Pubkey {
         s.len() <= 44,
         "Public key string length should be no more than 44"
     );
+    assert!(s.len() > 0, "Public key string cannot be empty");
 
     let map = get_base58ch_to_number_map();
     let mut bytes = [0u8; 32];
